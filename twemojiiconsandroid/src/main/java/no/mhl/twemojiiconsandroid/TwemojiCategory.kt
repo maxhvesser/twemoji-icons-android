9f102f5c85b4c3f20d1eaf31c3520ec3326c7197
Maximilian Hvesser-Lewis <maximilian.hvesser-lewis@vipps.no>
2021-08-02 23:27:46 +0200

    Began adding emoji as icons

---
+++ twemojiiconsandroid/src/main/java/no/mhl/twemojiiconsandroid/TwemojiCategory.kt
@@ -0,0 +1,14 @@
+package no.mhl.twemojiiconsandroid
+
+enum class TwemojiCategory {
+    Activities,
+    AnimalsAndNature,
+    Component,
+    Flags,
+    FoodAndDrink,
+    Objects,
+    PeopleAndBody,
+    SmileysAndEmotion,
+    Symbols,
+    TravelAndPlaces
+}
