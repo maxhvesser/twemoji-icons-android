7ab2edcf3439a8d9388c57061e7b365d5b63f463
Maximilian Hvesser-Lewis <maximilian.hvesser-lewis@vipps.no>
2021-08-26 17:35:15 +0200

    Removed old PeopleAndBody resources

---
+++ people/src/main/java/no/mhl/twemojiiconsandroid/people/PeopleAndBody.kt
@@ -0,0 +1,15 @@
+package no.mhl.twemojiiconsandroid.people
+
+import no.mhl.twemojiiconsandroid.core.model.EmojiIcon
+
+object PeopleAndBody {
+
+    // region Hand fingers open
+    private val handFingersOpen: List<EmojiIcon> = listOf()
+    // endregion
+
+    // region All
+    val all: List<EmojiIcon> = listOf()
+    // endregion
+
+}
