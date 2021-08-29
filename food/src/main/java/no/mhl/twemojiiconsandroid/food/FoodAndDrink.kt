3b1417e883b7fe9992e8543f597e757cd36c78ed
Maximilian Hvesser-Lewis <maximilian.hvesser-lewis@vipps.no>
2021-08-29 07:32:29 +0200

    Added foodFruit subcategory

--- food/src/main/java/no/mhl/twemojiiconsandroid/food/FoodAndDrink.kt
+++ food/src/main/java/no/mhl/twemojiiconsandroid/food/FoodAndDrink.kt
@@ -1,15 +1,40 @@
 package no.mhl.twemojiiconsandroid.food
 
+import no.mhl.twemojiiconsandroid.core.model.Category
 import no.mhl.twemojiiconsandroid.core.model.EmojiIcon
+import no.mhl.twemojiiconsandroid.core.model.Font
+import no.mhl.twemojiiconsandroid.core.model.Subcategory
 
 object FoodAndDrink {
 
     // region Food fruit
-    private val foodFruit: List<EmojiIcon> = listOf()
+    private val foodFruit: List<EmojiIcon> = listOf(
+        EmojiIcon("Grapes", "1f347", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f347),
+        EmojiIcon("Melon", "1f348", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f348),
+        EmojiIcon("Watermelon", "1f349", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f349),
+        EmojiIcon("Tangerine", "1f34a", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f34a),
+        EmojiIcon("Lemon", "1f34b", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f34b),
+        EmojiIcon("Banana", "1f34c", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f34c),
+        EmojiIcon("Pineapple", "1f34d", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f34d),
+        EmojiIcon("Mango", "1f96d", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f96d),
+        EmojiIcon("RedApple", "1f34e", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f34e),
+        EmojiIcon("GreenApple", "1f34f", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f34f),
+        EmojiIcon("Pear", "1f350", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f350),
+        EmojiIcon("Peach", "1f351", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f351),
+        EmojiIcon("Cherries", "1f352", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f352),
+        EmojiIcon("Strawberry", "1f353", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f353),
+        EmojiIcon("Blueberries", "1fad0", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1fad0),
+        EmojiIcon("KiwiFruit", "1f95d", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f95d),
+        EmojiIcon("Tomato", "1f345", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f345),
+        EmojiIcon("Olive", "1fad2", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1fad2),
+        EmojiIcon("Coconut", "1f965", Category.FoodAndDrink, Subcategory.FoodFruit, Font.Twemoji, R.drawable.ic_1f965)
+    )
     // endregion
 
     // region All
-    val all: List<EmojiIcon> = listOf()
+    val all: List<EmojiIcon> = listOf(
+        foodFruit
+    ).flatten()
     // endregion
 
 }
