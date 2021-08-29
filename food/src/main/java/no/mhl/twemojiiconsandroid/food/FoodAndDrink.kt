036461b78d734a623767e53e55614433977590b2
Maximilian Hvesser-Lewis <maximilian.hvesser-lewis@vipps.no>
2021-08-29 07:22:58 +0200

    Created FoodAndDrink class

---
+++ food/src/main/java/no/mhl/twemojiiconsandroid/food/FoodAndDrink.kt
@@ -0,0 +1,15 @@
+package no.mhl.twemojiiconsandroid.food
+
+import no.mhl.twemojiiconsandroid.core.model.EmojiIcon
+
+object FoodAndDrink {
+
+    // region Food fruit
+    private val foodFruit: List<EmojiIcon> = listOf()
+    // endregion
+
+    // region All
+    val all: List<EmojiIcon> = listOf()
+    // endregion
+
+}
