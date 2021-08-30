83ad6ea103bb2364a3103276b39d6acfdd94be3e
Maximilian Hvesser-Lewis <maximilian.hvesser-lewis@vipps.no>
2021-08-30 07:44:58 +0200

    Added foodPrepared subcategory

--- food/src/main/java/no/mhl/twemojiiconsandroid/food/FoodAndDrink.kt
+++ food/src/main/java/no/mhl/twemojiiconsandroid/food/FoodAndDrink.kt
@@ -51,10 +51,50 @@ object FoodAndDrink {
     )
     // endregion
 
+    // region Food prepared
+    private val foodPrepared: List<EmojiIcon> = listOf(
+        EmojiIcon("Bread", "1f35e", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f35e),
+        EmojiIcon("Croissant", "1f950", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f950),
+        EmojiIcon("BaguetteBread", "1f956", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f956),
+        EmojiIcon("Flatbread", "1fad3", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1fad3),
+        EmojiIcon("Pretzel", "1f968", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f968),
+        EmojiIcon("Bagel", "1f96f", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f96f),
+        EmojiIcon("Pancakes", "1f95e", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f95e),
+        EmojiIcon("Waffle", "1f9c7", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f9c7),
+        EmojiIcon("CheeseWedge", "1f9c0", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f9c0),
+        EmojiIcon("MeatOnBone", "1f356", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f356),
+        EmojiIcon("PoultryLeg", "1f357", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f357),
+        EmojiIcon("CutOfMeat", "1f969", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f969),
+        EmojiIcon("Bacon", "1f953", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f953),
+        EmojiIcon("Hamburger", "1f354", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f354),
+        EmojiIcon("FrenchFries", "1f35f", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f35f),
+        EmojiIcon("Pizza", "1f355", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f355),
+        EmojiIcon("HotDog", "1f32d", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f32d),
+        EmojiIcon("Sandwich", "1f96a", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f96a),
+        EmojiIcon("Taco", "1f32e", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f32e),
+        EmojiIcon("Burrito", "1f32f", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f32f),
+        EmojiIcon("Tamale", "1fad4", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1fad4),
+        EmojiIcon("StuffedFlatbread", "1f959", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f959),
+        EmojiIcon("Falafel", "1f9c6", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f9c6),
+        EmojiIcon("Egg", "1f95a", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f95a),
+        EmojiIcon("Cooking", "1f373", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f373),
+        EmojiIcon("ShallowPanOfFood", "1f958", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f958),
+        EmojiIcon("PotOfFood", "1f372", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f372),
+        EmojiIcon("Fondue", "1fad5", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1fad5),
+        EmojiIcon("BowlWithSpoon", "1f963", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f963),
+        EmojiIcon("GreenSalad", "1f957", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f957),
+        EmojiIcon("Popcorn", "1f37f", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f37f),
+        EmojiIcon("Butter", "1f9c8", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f9c8),
+        EmojiIcon("Salt", "1f9c2", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f9c2),
+        EmojiIcon("CannedFood", "1f96b", Category.FoodAndDrink, Subcategory.FoodPrepared, Font.Twemoji, R.drawable.ic_1f96b)
+    )
+    // endregion
+
     // region All
     val all: List<EmojiIcon> = listOf(
         foodFruit,
-        foodVegetable
+        foodVegetable,
+        foodPrepared
     ).flatten()
     // endregion
 
