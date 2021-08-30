aaf854ea0bf339173215808ab9368a6ea8c39b66
Maximilian Hvesser-Lewis <maximilian.hvesser-lewis@vipps.no>
2021-08-30 17:35:44 +0200

    Added placeMap subcategory

---
+++ travel/src/main/java/no/mhl/twemojiiconsandroid/travel/TravelAndPlaces.kt
@@ -0,0 +1,26 @@
+package no.mhl.twemojiiconsandroid.travel
+
+import no.mhl.twemojiiconsandroid.core.model.EmojiIcon
+import no.mhl.twemojiiconsandroid.core.model.Category
+import no.mhl.twemojiiconsandroid.core.model.Subcategory
+import no.mhl.twemojiiconsandroid.core.model.Font
+
+object TravelAndPlaces {
+
+    // region Place map
+    private val placeMap: List<EmojiIcon> = listOf(
+        EmojiIcon("GlobeShowingEuropeAfrica", "1f30d", Category.TravelAndPlaces, Subcategory.PlaceMap, Font.Twemoji, R.drawable.ic_1f30d),
+        EmojiIcon("GlobeShowingAmericas", "1f30e", Category.TravelAndPlaces, Subcategory.PlaceMap, Font.Twemoji, R.drawable.ic_1f30e),
+        EmojiIcon("GlobeShowingAsiaAustralia", "1f30f", Category.TravelAndPlaces, Subcategory.PlaceMap, Font.Twemoji, R.drawable.ic_1f30f),
+        EmojiIcon("GlobeWithMeridians", "1f310", Category.TravelAndPlaces, Subcategory.PlaceMap, Font.Twemoji, R.drawable.ic_1f310),
+        EmojiIcon("WorldMap", "1f5fa", Category.TravelAndPlaces, Subcategory.PlaceMap, Font.Twemoji, R.drawable.ic_1f5fa),
+        EmojiIcon("MapOfJapan", "1f5fe", Category.TravelAndPlaces, Subcategory.PlaceMap, Font.Twemoji, R.drawable.ic_1f5fe),
+        EmojiIcon("Compass", "1f9ed", Category.TravelAndPlaces, Subcategory.PlaceMap, Font.Twemoji, R.drawable.ic_1f9ed)
+    )
+    // endregion
+
+    // region All
+    val all: List<EmojiIcon> = listOf()
+    // endregion
+
+}
