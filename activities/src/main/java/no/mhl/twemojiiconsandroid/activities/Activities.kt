ad5afb22889d301c18206a0da0cd32db7f0f550a
Maximilian Hvesser-Lewis <maximilian.hvesser-lewis@vipps.no>
2021-08-31 08:49:00 +0200

    Added event subcategory

---
+++ activities/src/main/java/no/mhl/twemojiiconsandroid/activities/Activities.kt
@@ -0,0 +1,40 @@
+package no.mhl.twemojiiconsandroid.activities
+
+import no.mhl.twemojiiconsandroid.core.model.EmojiIcon
+import no.mhl.twemojiiconsandroid.core.model.Category
+import no.mhl.twemojiiconsandroid.core.model.Subcategory
+import no.mhl.twemojiiconsandroid.core.model.Font
+
+object Activities {
+
+    // region Event
+    private val event: List<EmojiIcon> = listOf(
+        EmojiIcon("JackOLantern", "1f383", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f383),
+        EmojiIcon("ChristmasTree", "1f384", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f384),
+        EmojiIcon("Fireworks", "1f386", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f386),
+        EmojiIcon("Sparkler", "1f387", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f387),
+        EmojiIcon("Firecracker", "1f9e8", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f9e8),
+        EmojiIcon("Sparkles", "2728", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_2728),
+        EmojiIcon("Balloon", "1f388", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f388),
+        EmojiIcon("PartyPopper", "1f389", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f389),
+        EmojiIcon("ConfettiBall", "1f38a", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f38a),
+        EmojiIcon("TanabataTree", "1f38b", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f38b),
+        EmojiIcon("PineDecoration", "1f38d", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f38d),
+        EmojiIcon("JapaneseDolls", "1f38e", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f38e),
+        EmojiIcon("CarpStreamer", "1f38f", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f38f),
+        EmojiIcon("WindChime", "1f390", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f390),
+        EmojiIcon("MoonViewingCeremony", "1f391", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f391),
+        EmojiIcon("RedEnvelope", "1f9e7", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f9e7),
+        EmojiIcon("Ribbon", "1f380", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f380),
+        EmojiIcon("WrappedGift", "1f381", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f381),
+        EmojiIcon("ReminderRibbon", "1f397", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f397),
+        EmojiIcon("AdmissionTickets", "1f39f", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f39f),
+        EmojiIcon("Ticket", "1f3ab", Category.Activities, Subcategory.Event, Font.Twemoji, R.drawable.ic_1f3ab)
+    )
+    // endregion
+
+    // region All
+    val all: List<EmojiIcon> = listOf()
+    // endregion
+
+}
