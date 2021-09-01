bf4e418318f90ddf6811dbc5f8a681a6f735bd66
Maximilian Hvesser-Lewis <maximilian.hvesser-lewis@vipps.no>
2021-09-01 15:26:36 +0200

    Created Symbols class

---
+++ symbols/src/main/java/no/mhl/twemojiiconsandroid/symbols/Symbols.kt
@@ -0,0 +1,14 @@
+package no.mhl.twemojiiconsandroid.symbols
+
+import no.mhl.twemojiiconsandroid.core.model.EmojiIcon
+import no.mhl.twemojiiconsandroid.core.model.Category
+import no.mhl.twemojiiconsandroid.core.model.Subcategory
+import no.mhl.twemojiiconsandroid.core.model.Font
+
+object Symbols {
+
+    // region All
+    val all: List<EmojiIcon> = listOf()
+    // endregion
+
+}
