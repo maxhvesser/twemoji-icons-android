ec00f089aaaf075467c4991e6c4c61368d17128e
Maximilian Hvesser-Lewis <maximilian.hvesser-lewis@vipps.no>
2021-09-01 18:17:26 +0200

    Added flag subcategory

--- flags/src/main/java/no/mhl/twemojiiconsandroid/flags/Flags.kt
+++ flags/src/main/java/no/mhl/twemojiiconsandroid/flags/Flags.kt
@@ -7,8 +7,22 @@ import no.mhl.twemojiiconsandroid.core.model.Font
 
 object Flags {
 
+    // region Flag
+    private val flag: List<EmojiIcon> = listOf(
+        EmojiIcon("ChequeredFlag", "1f3c1", Category.Flags, Subcategory.Flag, Font.Twemoji, R.drawable.ic_1f3c1),
+        EmojiIcon("TriangularFlag", "1f6a9", Category.Flags, Subcategory.Flag, Font.Twemoji, R.drawable.ic_1f6a9),
+        EmojiIcon("CrossedFlags", "1f38c", Category.Flags, Subcategory.Flag, Font.Twemoji, R.drawable.ic_1f38c),
+        EmojiIcon("BlackFlag", "1f3f4", Category.Flags, Subcategory.Flag, Font.Twemoji, R.drawable.ic_1f3f4),
+        EmojiIcon("WhiteFlag", "1f3f3", Category.Flags, Subcategory.Flag, Font.Twemoji, R.drawable.ic_1f3f3),
+        EmojiIcon("RainbowFlag", "1f3f3-fe0f-200d-1f308", Category.Flags, Subcategory.Flag, Font.Twemoji, R.drawable.ic_1f3f3_fe0f_200d_1f308),
+        EmojiIcon("PirateFlag", "1f3f4-200d-2620-fe0f", Category.Flags, Subcategory.Flag, Font.Twemoji, R.drawable.ic_1f3f4_200d_2620_fe0f)
+    )
+    // endregion
+
     // region All
-    val all: List<EmojiIcon> = listOf()
+    val all: List<EmojiIcon> = listOf(
+        flag
+    ).flatten()
     // endregion
 
 }
