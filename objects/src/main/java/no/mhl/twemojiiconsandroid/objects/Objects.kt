daa777a9622c0374098f88d8d5d4fd998bf98e16
Maximilian Hvesser-Lewis <maximilian.hvesser-lewis@vipps.no>
2021-09-01 08:46:19 +0200

    Created Objects class

---
+++ objects/src/main/java/no/mhl/twemojiiconsandroid/objects/Objects.kt
@@ -0,0 +1,14 @@
+package no.mhl.twemojiiconsandroid.objects
+
+import no.mhl.twemojiiconsandroid.core.model.EmojiIcon
+import no.mhl.twemojiiconsandroid.core.model.Category
+import no.mhl.twemojiiconsandroid.core.model.Subcategory
+import no.mhl.twemojiiconsandroid.core.model.Font
+
+object Objects {
+
+    // region All
+    val all: List<EmojiIcon> = listOf()
+    // endregion
+
+}
