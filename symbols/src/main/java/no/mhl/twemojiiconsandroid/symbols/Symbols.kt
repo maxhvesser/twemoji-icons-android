16965dd6f881f5c5179c8f38f7ca46e85d297526
Maximilian Hvesser-Lewis <maximilian.hvesser-lewis@vipps.no>
2021-09-02 07:24:41 +0200

    Optimized imports

--- symbols/src/main/java/no/mhl/twemojiiconsandroid/symbols/Symbols.kt
+++ symbols/src/main/java/no/mhl/twemojiiconsandroid/symbols/Symbols.kt
@@ -1,9 +1,9 @@
 package no.mhl.twemojiiconsandroid.symbols
 
-import no.mhl.twemojiiconsandroid.core.model.EmojiIcon
 import no.mhl.twemojiiconsandroid.core.model.Category
-import no.mhl.twemojiiconsandroid.core.model.Subcategory
+import no.mhl.twemojiiconsandroid.core.model.EmojiIcon
 import no.mhl.twemojiiconsandroid.core.model.Font
+import no.mhl.twemojiiconsandroid.core.model.Subcategory
 
 object Symbols {
 
